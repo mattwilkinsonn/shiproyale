7a7dd21d6c902c8c19d65678eb15cf4fcb5faa93
Matt Wilkinson <matt@mattwilkinson.dev>
2023-12-07 16:12:53 -0500

    first commit

---
+++ Player.gd
@@ -0,0 +1,60 @@
+extends CharacterBody2D
+
+const ProjectileScene = preload("res://Projectile.tscn")
+
+@export var MOVEMENT_SPEED = 100
+@export var ROTATIONAL_VELOCITY = 90.0
+
+var ROTATIONAL_VELOCITY_RADIANS = deg_to_rad(ROTATIONAL_VELOCITY)
+
+func _physics_process(delta: float):
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
+	set_movement(input_direction)
+	
+	var rotation_direction = Input.get_axis("Rotate Counterclockwise", "Rotate Clockwise")
+	calculate_rotation(delta, rotation_direction)
+
+	move_and_slide()
+
+func set_movement(input_direction: Vector2):
+	velocity = input_direction * MOVEMENT_SPEED
+	
+func calculate_rotation(delta: float, rotation_direction: float):
+	var current_angle = rotation
+	
+	var angle_change = get_angle_change(delta, rotation_direction)
+	
+	var new_angle = current_angle + angle_change
+	rotation = lerp_angle(current_angle, new_angle, 1)
+
+func get_angle_change(delta: float, rotation_direction: float):
+	return ROTATIONAL_VELOCITY_RADIANS * delta * rotation_direction
+	
+
+
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("Fire"):
+		fire_action()
+		
+func fire_action():
+	# get current rotation
+	var left_fire_direction = global_rotation - (PI / 2)
+	var right_fire_direction = global_rotation + (PI / 2)
+	
+	fire_projectile(left_fire_direction)
+	fire_projectile(right_fire_direction)
+	
+func fire_projectile(fire_direction: float):
+	var projectile = ProjectileScene.instantiate()
+	
+	var fire_vector = Vector2.UP.rotated(fire_direction)
+	
+ 	# TODO: get edge of ship's collison box and position there
+	projectile.position = global_position + (fire_vector * 20)
+	projectile.apply_central_impulse(fire_vector * 150)
+	get_parent().add_child(projectile)
+	
+	
+	
+	
