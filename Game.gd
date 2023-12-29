c2ea7c02a0f2abf09317fcdb94f0adc189db031c
Matt <mattwilki17@gmail.com>
2023-12-29 18:09:21 -0500

    fire projectiles multiplayer
    Signed-off-by: Matt <mattwilki17@gmail.com>

--- Game.gd
+++ Game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 const PlayerScene = preload("res://Player.tscn")
+const ProjectileScene = preload("res://Projectile.tscn")
 
 func _ready():
 	# Preconfigure game.
+	$ProjectileSpawner.set_spawn_function(projectile_spawn)
 	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
 
 var player_characters = {}
@@ -17,14 +19,26 @@ func start_game():
 		
 @rpc("authority", "call_local", "reliable", 2)
 func create_character(id: int):
-	var character = PlayerScene.instantiate()
-	player_characters[id] = character
-	character.global_position = $SpawnPoint.global_position
+	var player = PlayerScene.instantiate()
+	player_characters[id] = player
+	player.global_position = $SpawnPoint.global_position
 	if multiplayer.get_unique_id() == id:
-		character.is_local_player = true
+		player.is_local_player = true
+	player.fire.connect(_on_player_fire)
+	add_child(player)
 
 
+func _on_player_fire(pos, impulse):
+	$ProjectileSpawner.spawn({"pos": pos, "impulse": impulse})
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func projectile_spawn(data):
+	var projectile = ProjectileScene.instantiate()
+	projectile.position = data.pos
+	projectile.apply_central_impulse(data.impulse)
+	return projectile
+	
--- Player.gd
+++ Player.gd
@@ -2,18 +2,33 @@ extends CharacterBody2D
 
 const ProjectileScene = preload("res://Projectile.tscn")
 
+signal fire(pos, impulse)
+
 @export var MOVEMENT_SPEED = 100
 @export var ROTATIONAL_VELOCITY = 90.0
 
 var ROTATIONAL_VELOCITY_RADIANS = deg_to_rad(ROTATIONAL_VELOCITY)
 var is_local_player = false
 
-func _physics_process(delta: float):
+var current_movement_input = Vector2.ZERO
+var current_rotation_input = 0
+
+func _process(delta: float):
+	if not is_local_player:
+		return
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	set_movement(input_direction)
-	
 	var rotation_direction = Input.get_axis("Rotate Counterclockwise", "Rotate Clockwise")
-	calculate_rotation(delta, rotation_direction)
+	receive_player_movement_input.rpc_id(1, input_direction, rotation_direction)
+	
+	
+@rpc("any_peer", "call_remote", "unreliable_ordered")
+func receive_player_movement_input(input_direction: Vector2, rotation_direction: float):
+	current_movement_input = input_direction
+	current_rotation_input = rotation_direction
+	
+func _physics_process(delta: float):
+	set_movement(current_movement_input)
+	calculate_rotation(delta, current_rotation_input)
 
 	move_and_slide()
 
@@ -35,9 +50,12 @@ func get_angle_change(delta: float, rotation_direction: float):
 
 
 func _input(event: InputEvent):
+	if not is_local_player:
+		return
 	if event.is_action_pressed("Fire"):
-		fire_action()
+		fire_action.rpc_id(1)
 		
+@rpc("any_peer", "call_remote", "unreliable")
 func fire_action():
 	# get current rotation
 	var left_fire_direction = global_rotation - (PI / 2)
@@ -45,16 +63,17 @@ func fire_action():
 	
 	fire_projectile(left_fire_direction)
 	fire_projectile(right_fire_direction)
-	
+
 func fire_projectile(fire_direction: float):
 	var projectile = ProjectileScene.instantiate()
 	
 	var fire_vector = Vector2.UP.rotated(fire_direction)
 	
  	# TODO: get edge of ship's collison box and position there
-	projectile.position = global_position + (fire_vector * 20)
-	projectile.apply_central_impulse(fire_vector * 150)
-	get_parent().add_child(projectile)
+	var pos = global_position + (fire_vector * 20)
+	var impulse = fire_vector * 150
+	fire.emit(pos, impulse)
+
 	
 	
 	
